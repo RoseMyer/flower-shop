a02240a2f4bb4b11bad7a6b0d13d79bd1b761c22
Rose Myer <rosemyer@roses-mbp.mynetworksettings.com>
2024-05-10 13:28:22 -0400

    added tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AppRouterCacheProvider>
+        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
           {children}
         </AppRouterCacheProvider>
       </body>
