bb94ebfb870ead82c79db5459d282df8f0c064e4
Rose Myer <rosemyer@roses-mbp.mynetworksettings.com>
2024-05-11 06:44:34 -0400

    ses

--- utils/sendEmail.ts
+++ utils/sendEmail.ts
@@ -1,36 +1,35 @@
-import AWS from 'aws-sdk';
+"use server"
 
-// Configure the AWS region and credentials
-AWS.config.update({ region: 'us-east-2' }); // Make sure to use the correct region
+import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
 
-const ses = new AWS.SES();
+const sesClient = new SESClient({ region: "us-east-2" });
 
 export const sendEmail = async (customerData: any, orderData: any) => {
-    const email_address = customerData.email
-    console.log('email address: ', email_address)
-  const params = {
-    Source: 'rosesmyer@icloud.com',  // Use your verified email address
-    Destination: {
-      ToAddresses: [email_address]
-    },
-    Message: {
-      Subject: {
-        Data: 'Order Confirmation'
-      },
-      Body: {
-        Text: {
-          Data: JSON.stringify(orderData)
+    const params = {
+        Source: 'rosesmyer@icloud.com',
+        Destination: {
+            ToAddresses: [customerData.email]
+        },
+        Message: {
+            Subject: {
+                Data: 'Order Confirmation'
+            },
+            Body: {
+                Text: {
+                    Data: JSON.stringify(orderData)
+                }
+            }
         }
-      }
-    }
-  };
+    };
 
-  try {
-    const response = await ses.sendEmail(params).promise();
-    console.log("Email sent:", response);
-    return true;
-  } catch (error) {
-    console.error("Failed to send email:", error);
-    return false;
-  }
+    const command = new SendEmailCommand(params);
+
+    try {
+        const response = await sesClient.send(command);
+        console.log("Email sent:", response);
+        return true;
+    } catch (error) {
+        console.error("Failed to send email:", error);
+        return false;
+    }
 };
